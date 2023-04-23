5b62f2715e57f7afd581187a39018cbfd83719db
Aliaksandr Naumavets <naum1979alex@gmail.com>
2023-04-23 12:24:52 +0300

    fix: to do item edit mode stopped working correctly after point 3.2 commit

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".to-do-item__btn-edit-save");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("to-do-item_edit");
     //If class of the parent is .edit-mode
     if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("to-do-item_edit");
 };
 
 
